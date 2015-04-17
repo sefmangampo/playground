214520869619db3fcdca6cbe3db4200a15242549
sefmangampo <sefmangampo@gmail.com>
2015-04-17 21:00:27 +0800

    movers

--- js/basic.js
+++ js/basic.js
@@ -1,26 +1,37 @@
 var mainWidth;
 var mainHeight;
 var show= false;
+var showFooterView = false;
 
 var main = function(){
 	getWindowDimensions();
 	
 	listeners();
-	
+
 	$(window).resize(getWindowDimensions);
 }
 function listeners(){
 	
-	$('.navItem').click(function(event){
-		
+	$('footer li').click(function(){
+		//alert($(this).attr('value'));
+		showFooterView = !showFooterView;
+		if(showFooterView){
+			TweenLite.to($('.footerView'),1.5,{right:0,ease:"Elastic.easeOut"});
+		}else{
+			TweenLite.to($('.footerView'),1.5,{right: -mainWidth * 0.3,ease:"Elastic.easeOut"});
+		}
+	
+	});
+	
+	
+	$('.navItem').click(function(event){	
 		show = !show;
 		if(show){
 			TweenLite.to($('#view'),0.5,{autoAlpha:0.9});
 			TweenLite.to($('#view2'),0.5,{autoAlpha:0});
 		}else{
 			TweenLite.to($('#view'),0.5,{autoAlpha:0});
-			TweenLite.to($('#view2'),0.5,{autoAlpha:1});
-		
+			TweenLite.to($('#view2'),0.5,{autoAlpha:1});		
 		}
 	});
 	
@@ -30,6 +41,7 @@ function getWindowDimensions(){
 	mainHeight = $(window).height();
 	
 	movers();
+	
 }
 
 function movers(){
@@ -39,7 +51,9 @@ function movers(){
 		TweenLite.to(this,0.5,{left:-100});
 	});
 	
-
+	$('footer').css({height:mainHeight *0.06,width:mainWidth * 0.9, left: mainWidth * 0.05});
+	$('header').css({height:mainHeight *0.06,width:mainWidth * 0.9, left: mainWidth * 0.05});
+	$('.footerView').css({height: mainHeight * 0.8,width:mainWidth * 0.3,top:mainHeight * 0.1,right: -mainWidth * 0.3});
 	
 	TweenLite.to($('#view,#view2'),0.5,{width:mainWidth * 0.75,left:mainWidth * 0.125, top: mainHeight * 0.1,height:mainHeight * 0.5});
 	
